889ba93cc08e68713795b74b84c9b4b95e68e1a2
Clarence Biggans <clarencebiggans@gmail.com>
2019-06-03 08:53:38 -0700

    Add in the other text fields in MarkForm

--- src/components/MarkForm.js
+++ src/components/MarkForm.js
@@ -9,6 +9,31 @@ function MarkForm(props) {
              value={props.nextMark.url}
              placeholder='URL'
              onChange={props.handleChange} />
+      <input type='text'
+             name='title'
+             value={props.nextMark.title}
+             placeholder='Title'
+             onChange={props.handleChange} />
+      <input type='text'
+             name='description'
+             value={props.nextMark.description}
+             placeholder='Description'
+             onChange={props.handleChange} />
+      <input type='text'
+             name='tags'
+             value={props.nextMark.tags}
+             placeholder='Tags'
+             onChange={props.handleChange} />
+      <input type='text'
+             name='category'
+             value={props.nextMark.category}
+             placeholder='Category'
+             onChange={props.handleChange} />
+      <input type='text'
+             name='type'
+             value={props.nextMark.type}
+             placeholder='Type'
+             onChange={props.handleChange} />
       <button type='submit'>ADD</button>
     </form>
   )
